b772bc835c83715628b6efa50feb5c351b6a9f19
Trix <Trix@DESKTOP-QAEJLQP>
2026-07-01 11:25:00 -0400

    Updated gitignore, hopefully correctly

--- Main.cpp
+++ Main.cpp
@@ -313,7 +313,7 @@ int main()
 			// TheFirst(2);
 			// Hydra(4, 14);
 			// Deepchad(4);
-			AlphaCruncher(120);
+			AlphaCruncher(60);
 
 		std::bitset<3> gamemode(0b010); /*Gates, Bases, Ports*/
 		gameMaster = new GameMaster(gamemode, p1, p2, 10000, 0, &displayBoard[0]);
@@ -327,7 +327,7 @@ int main()
 		// gameMaster->loadPos("b-10000 r-10006 b-10012 -B10500 -B10502 -W10510 -W10512 b-10606 -B10700 -B10702 -W10710 -W10712 b-11200 r-11206 b-11212 11111111111111111111");
 		// gameMaster->loadPos("b-10000 r-10006 b-10012 -W10409 -B10502 bW20603 -W20710 -B10803 -B10900 b-11200 r-11206 b-11212 11111111111111111111");
 		// gameMaster->loadPos("-B10500 -W10602 11111111111111111111");
-		// gameMaster->loadPos("b-10000 r-10006 b-10012 -W30510 -B30502 b-10606 -B30702 -W30710 b-11200 r-11206 b-11212 11111111111111111111"); // slaughterhouse
+		gameMaster->loadPos("b-10000 r-10006 b-10012 -W30510 -B30502 b-10606 -B30702 -W30710 b-11200 r-11206 b-11212 11111111111111111111"); // slaughterhouse
 		// gameMaster->loadPos("b-10000 r-10006 b-10012 -W30609 -B30603 b-11200 r-11206 b-11212 11111111111111111111");
 		// gameMaster->loadPos("b-10002 b-10005 -B10102 -W20308 rW30404 -B10500 -B10512 -B10602 -W20604 b-10607 r-10608 -W30610 -B10700 b-10708 -W10804 -W10912 -B11100 -B11110 11111100101101111111");
 		/// gameMaster->loadPos("r-10006 bW40112 -B30202 -B20300 -B10302 bB20400 -B10402 -W30411 -W20508 bW20606 -W30610 -B20801 -B20902 b-11200 r-11206 b-11212 00000000000100000000");
